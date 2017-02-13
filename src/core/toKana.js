6513de44eb242767ce3e69137c710b33e5a76db1
DJTB <duncan.bay@gmail.com>
2017-02-13 11:33:21 +0530

    build: swap to husky for prepush hooks instead of precommit only [skip ci]

--- src/core/toKana.js
+++ src/core/toKana.js
@@ -1,5 +1,17 @@
 import romajiToKana from './romajiToKana';
 
+/**
+ * Converts input to kana.
+ * Uses romajiToKana under the hood
+ * @param  {String} input text to convert
+ * @param  {Object} options
+ * @return {String} converted text
+ * @example
+ * toKana('hiragana')
+ * // => 'ひらがな'
+ * toKana('KATAKANA')
+ * // => 'カタカナ'
+ */
 function toKana(input, options) {
   return romajiToKana(input, options);
 }
